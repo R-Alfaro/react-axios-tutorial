3d149eb22b114b1fd81cf39d7b3704d360aadad4
RAlfaro <rkalfaro@gmail.com>
2020-12-17 23:07:18 -0800

    final code changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import API from "./utils/API";
+import User from "./utils/User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      name: null,
+      avatar: null,
+      email: null
+    };
+  }
+
+  async componentDidMount() {
+    
+    try {
+      // Load async data.
+      let userData = await API.get('/', {
+        params: {
+          results: 1,
+          inc: 'name, email, picture'
+        }
+      })
+
+      userData = userData.data.results[0];
+
+      // Parse the results for ease of use.
+      const name = `${userData.name.first} ${userData.name.last}`;
+      const avatar = userData.picture.large;
+      const email = userData.email;
+
+      this.setState(
+        {...this.state, ...{
+          isLoading: false, name, avatar, email }
+        }
+      );
+    } catch (error) {
+      console.log(`😱 Axios request failed: ${error}`);
+    }
+  }
+
+  render() {
+    const { isLoading, name, avatar, email } = this.state;
+
+    return (
+      <User isLoading={isLoading} name={name} avatar={avatar} email={email} />
+    );
+  }
 }
 
 export default App;
